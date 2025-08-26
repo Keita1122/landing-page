4b29b0dc8af975a12525a4734ed928e48d85dff2
Keita1122 <arc.k.agu1122t@gmail.com>
2025-08-26 20:58:11 +0900

    ベース作成完了

---
+++ homepage/src/app/salesfinder-home/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./styles.module.scss";
+
+
+export default function Home (){
+
+  return(
+    <div className={styles.page}>
+      <p>test</p>
+    </div>
+
+  )
+};
