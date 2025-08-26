0791f890ad499193372b19140b3b11389f20dcb4
Keita1122 <arc.k.agu1122t@gmail.com>
2025-08-26 23:08:57 +0900

    ヘッダー編集

--- homepage/src/app/salesfinder-home/page.tsx
+++ homepage/src/app/salesfinder-home/page.tsx
@@ -1,12 +1,24 @@
+'use client'
 import React from 'react';
 import styles from "./styles.module.scss";
-
+import Image from "next/image";
+import Logo from "../../../public/new-logo.png";
 
 export default function Home (){
 
   return(
     <div className={styles.page}>
-      <p>test</p>
+      <div className={styles.header}>
+        <button><Image src={Logo} alt="ロゴ" className={styles.logo}/></button>
+        <nav>
+          <ul>
+            <li>会社概要</li>
+            <li>事業内容</li>
+            <li>会社概要</li>
+            <li>お問い合わせ</li>
+          </ul>
+        </nav>
+      </div>
     </div>
 
   )
