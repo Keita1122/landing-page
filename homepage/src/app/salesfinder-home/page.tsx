3f2b3528b7257d3211aea8102ebaed206fa1934d
Keita1122 <arc.k.agu1122t@gmail.com>
2025-08-27 16:38:22 +0900

    コンポーネント編集

--- homepage/src/app/salesfinder-home/page.tsx
+++ homepage/src/app/salesfinder-home/page.tsx
@@ -1,8 +1,10 @@
 'use client'
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./styles.module.scss";
 import Image from "next/image";
 import Logo from "../../../public/new-logo.png";
+
+//import CircularNavigation from '@/components/CIrcularNavigation';
 
 export default function Home (){
 
@@ -18,6 +20,10 @@ export default function Home (){
             <li>お問い合わせ</li>
           </ul>
         </nav>
+      </div>
+
+      <div className={styles.main}>
+        
       </div>
     </div>
 
---
+++ homepage/src/components/MainScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from "./MainScreen.module.scss";
+
+const MainScreen = () => {
+
+  return(
+
+    <div className={styles.mainContents}>
+
+
+  </div>
+  );
+};
+export default MainScreen;
